547a0c2e6c5528af64c74206ab02148afa7f73ee
Robin Willems <r.willems@tue.nl>
2024-11-19 17:05:53 +0100

    Adjusted name fitting module.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = """
 Vtnurbs is a geometry constructor and class that is specifically developed for idealized ventricular anatomy. 
 It employs Non-Uniform Rational B-Splines (NURBS) to construct a multipatch geometry of either a left or a 
 bi-ventricle geometry. The geometry is used as a basis for the CardIGA module (IGA-based cardiac model) and
-the EBDM module (Error-based Deformation Method).
+the multipatch fitting module.
 """
 with open(os.path.join('vtnurbs', '__init__.py')) as f:
   version = next(filter(None, map(re.compile("^version = '([a-zA-Z0-9.]+)'$").match, f))).group(1)
